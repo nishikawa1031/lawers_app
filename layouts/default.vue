5556ea3076b74c176c8ba200fc28912aeb21319c
tanishi <tatsunishi1031@yahoo.co.jp>
2020-05-20 20:38:45 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app dark>
+    <v-app-bar>
+      <v-btn text to="/"><v-toolbar-title v-text="title" /></v-btn>
+      <v-btn
+        v-for="(subject, index) in subjects"
+        :key="index"
+        text
+        @click="subjectID(subject.id)"
+      >
+        {{ subject.content }}
+      </v-btn>
+    </v-app-bar>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+    <v-btn text>contact</v-btn>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      subjects: [
+        { id:0, content: '憲法', created: '2019-03-31 15:30' },
+        { id:1, content: '民法', created: '2019-03-31 15:30' },
+        { id:2, content: '刑法', created: '2019-03-31 15:30' },
+        { id:3, content: '民訴', created: '2019-03-31 15:30' },
+        { id:4, content: '刑訴', created: '2019-03-31 15:30' },
+        { id:5, content: '行政法', created: '2019-03-31 15:30' },
+        { id:6, content: '商法', created: '2019-03-31 15:30' },
+        { id:7, content: '労働法', created: '2019-03-31 15:30' },
+        { id:8, content: '知財', created: '2019-03-31 15:30' },
+        { id:9, content: '倒産', created: '2019-03-31 15:30' },
+        { id:10, content: '租税', created: '2019-03-31 15:30' },
+        { id:11, content: '環境法', created: '2019-03-31 15:30' },
+        { id:12, content: '国際関係法', created: '2019-03-31 15:30' },
+      ],
+      title: '司法試験答案集'
+    }
+  },
+  methods:{
+    subjectID(id){
+      this.$router.push({path:'/subject/id/',query:{'id':id}})
+    }
+  }
+}
+</script>
---
+++ pages/subject/id.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card>
+        <v-card-title class="headline">
+          年度を選択してください。
+        </v-card-title>
+        <v-card-text>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            v-for="(year, index) in years"
+            :key="index"
+            text
+            @click="yearID(year.id)"
+          >
+            {{ year.content }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      years: [
+        { id:2019, content: '平成31年', created: '2019-03-31 15:30' },
+        { id:2018, content: '平成30年', created: '2019-03-31 15:30' },
+        { id:2017, content: '平成29年', created: '2019-03-31 15:30' },
+        { id:2016, content: '平成28年', created: '2019-03-31 15:30' },
+        { id:2015, content: '平成27年', created: '2019-03-31 15:30' },
+        { id:2014, content: '平成26年', created: '2019-03-31 15:30' },
+        { id:2013, content: '平成25年', created: '2019-03-31 15:30' },
+        { id:2012, content: '平成24年', created: '2019-03-31 15:30' },
+        { id:2011, content: '平成23年', created: '2019-03-31 15:30' },
+        { id:2010, content: '平成22年', created: '2019-03-31 15:30' },
+        { id:2009, content: '平成21年', created: '2019-03-31 15:30' },
+        { id:2008, content: '平成19年', created: '2019-03-31 15:30' },
+        { id:2007, content: '平成18年', created: '2019-03-31 15:30' },
+      ]
+    }
+  },
+  methods:{
+    yearID(id){
+      this.selected = this.route.params.id;
+      this.$router.push({path:'/subject/id/' + this.selected + 'year/year_id/',query:{'year_id':year_id}})
+    }
+  }
+}
+</script>
+
